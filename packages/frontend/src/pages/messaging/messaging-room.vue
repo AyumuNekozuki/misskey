488cf3db8285f023f81119f4c8ba4d7c11ed4501
AyumuNekozuki <ayumu@nekozuki.me>
2023-01-17 17:05:19 +0900

    fix Icon, change Version

--- packages/frontend/src/pages/messaging/messaging-room.vue
+++ packages/frontend/src/pages/messaging/messaging-room.vue
@@ -9,7 +9,7 @@
 		<MkPagination v-if="pagination" ref="pagingComponent" :key="userAcct || groupId" :pagination="pagination">
 			<template #empty>
 				<div class="_fullinfo">
-					<img src="https://xn--931a.moe/assets/info.jpg" class="_ghost"/>
+					<img src="https://files.mi.nekozuki.me/mashiro/mashiro_bg_256.png" class="_ghost"/>
 					<div>{{ i18n.ts.noMessagesYet }}</div>
 				</div>
 			</template>
